bcb73e03a1f177dd2c3df9ba690f98f80171c361
Andrii Verbovetskyi <verbov@Andriis-MacBook-Pro.local>
2017-03-04 21:49:09 +0200

    added filter to add issuer vc

--- KAPU/AddKapuTableVC.swift
+++ KAPU/AddKapuTableVC.swift
@@ -8,10 +8,22 @@
 
 import UIKit
 
+enum State {
+    case table
+    case filter
+}
+
+let filterCellNames = ["Infrastructure", "Transportation", "Decision Making"]
+
 class AddKapuVC: UIViewController {
     
+    @IBOutlet weak var titleButton: UIButton!
     @IBOutlet var table: UITableView!
     var numberOfOptions = 3
+    var state:State = .table
+    @IBOutlet weak var filterTable: UITableView!
+    @IBOutlet weak var filterBackgroundView: UIView!
+    var selectedFilter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,8 +33,33 @@ class AddKapuVC: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
-
+    func openFilter(){
+        state = .filter
+        filterBackgroundView.isHidden = false
+        view.bringSubview(toFront: filterBackgroundView)
+    }
     
+    func closeFilter(){
+        state = .table
+        filterBackgroundView.isHidden = true
+    }
+    
+    @IBAction func close(_ sender: Any) {
+        state = .table
+        filterBackgroundView.isHidden = true
+    }
+    @IBAction func openMenu(_ sender: Any) {
+        switch state {
+        case .table:
+            openFilter()
+            break
+        case .filter:
+            closeFilter()
+            break
+        default: break
+        }
+    }
+
 }
 
 extension AddKapuVC: UITableViewDelegate{
@@ -34,11 +71,25 @@ extension AddKapuVC: UITableViewDelegate{
         }
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if (tableView == filterTable){
+            selectedFilter = indexPath.row
+            titleButton.setTitle(filterCellNames[selectedFilter], for: .normal)
+            filterTable.reloadData()
+        } else {
+            numberOfOptions += 1
+            self.table.reloadData()
+        }
+    }
+    
 }
 
 extension AddKapuVC: UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        if (tableView == self.filterTable) {
+            return 3
+        }
         switch section {
         case 0, 1:
             return 1
@@ -50,11 +101,17 @@ extension AddKapuVC: UITableViewDataSource{
     
     
     func numberOfSections(in tableView: UITableView) -> Int {
+        if (tableView == self.filterTable) {
+            return 1
+        }
         return 3
     }
     
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if (tableView == self.filterTable) {
+            return ""
+        }
         switch section {
         case 0:
             return "Title"
@@ -68,7 +125,18 @@ extension AddKapuVC: UITableViewDataSource{
         
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         var cell: UITableViewCell?
-        
+        if (tableView == self.filterTable) {
+            let filterCell = tableView.dequeueReusableCell(withIdentifier: "FilterCell", for: indexPath) as! FilterTableViewCell
+            filterCell.categoryName.text = filterCellNames[indexPath.row]
+            if (indexPath.row == selectedFilter){
+                filterCell.changeColorIfSelecedView.isHidden = false
+                filterCell.categoryName.textColor = UIColor.white
+            } else {
+                filterCell.changeColorIfSelecedView.isHidden = true
+                filterCell.categoryName.textColor = UIColor.black
+            }
+            return filterCell
+        }
         switch(indexPath.section) {
         case 0:
             cell = tableView.dequeueReusableCell(withIdentifier: "textFieldCell", for: indexPath) as! TextFieldTableViewCell
@@ -95,11 +163,6 @@ extension AddKapuVC: UITableViewDataSource{
         default: break
         }
         return cell!
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        numberOfOptions += 1
-        self.table.reloadData()
     }
     
 }
